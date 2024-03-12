cf9cf906a5671fcbc343902a2b6621ebbe474ed3
beppumisato <lacuisinedemisato@gmail.com>
2024-03-12 11:04:44 +0900

    ログイン画面データ構造の追加

---
+++ prisma/migrations/20240312003309_init/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "Id" SERIAL NOT NULL,
+    "Email" TEXT NOT NULL,
+    "Name" TEXT,
+    "Password" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("Id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_Email_key" ON "User"("Email");
